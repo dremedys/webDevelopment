6beadac0bc77096f2a3b0850ad7fb301aa5872c4
Dariga <dariga200155@gmail.com>
2021-02-20 15:28:19 +0500

    bonusDone

--- week3/todoApp/app.js
+++ week3/todoApp/app.js
@@ -5,14 +5,25 @@ function toHTML(txt) {
         <button class="del-btn" data-btn="delete">X</button>
     `
 }
-
+shownDone = false
+function toHTMLDone(text){
+    return `<p>${text}</p>
+            <button class="del-btn" data-btn="delete">X</button>
+`
+}
+completed = []
 document.addEventListener('click', (event) => {
     const btnType = event.target.dataset.btn
     if (btnType == 'delete') {
         event.target.parentNode.remove()
     } else if (btnType == 'done') {
-        if (event.target.parentNode.getElementsByTagName('span')[0].style.textDecoration != 'line-through')
+        if (event.target.parentNode.getElementsByTagName('span')[0].style.textDecoration != 'line-through'){
             event.target.parentNode.getElementsByTagName('span')[0].style.textDecoration = 'line-through'
+            // completed.push(event.target.innerText)
+            completed.push(event.target.parentNode.getElementsByTagName('span')[0].textContent)
+            event.target.parentNode.remove()
+        }
+
         else
             event.target.parentNode.getElementsByTagName('span')[0].style.textDecoration = 'none'
     }
@@ -39,3 +50,24 @@ function addNode() {
     document.getElementById('list-content').appendChild(node)
     document.getElementById('input-text').value = ''
 }
+
+document.getElementById('remove').addEventListener('click',()=>{
+    document.getElementById('list-content').innerHTML = ''
+    document.getElementById('doneList').innerHTML = ''
+})
+
+document.getElementById('done').addEventListener('click',()=>{
+    let html = ``
+    completed.forEach(elem => {
+        html += toHTMLDone(elem)
+    })
+    if(!shownDone){
+        document.getElementById('doneList').innerHTML = html
+        document.getElementById('done').textContent = 'Hide done'
+        shownDone = true
+    }
+
+    else {document.getElementById('doneList').innerHTML=''
+    shownDone = false
+        document.getElementById('done').textContent = 'Show done'}
+})
